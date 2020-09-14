a7e439047cac42dd3aee14307d0104d06f37be00
Galih Fajar <galihfajar00@gmail.com>
2020-09-14 20:36:37 +0700

    fixing

--- index.js
+++ index.js
@@ -47,7 +47,8 @@ function handleEvent(event) {
     const return_tugas = async () => {
       try{
         const response = await axios.get("https://pyrgoose.firebaseio.com/tugas.json");
-        console.log(response, "ini response");
+        console.log(typeof response);
+        console.log("++++ diatas responsenya +++++");
         return client.replyMessage(event.replyToken, {
           type : 'text', text : "bisa ga sih gan elah"
         });
