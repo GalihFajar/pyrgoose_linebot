78750bbcd036201a757e68e586e69518f2f1c30a
Galih Fajar <galihfajar00@gmail.com>
2020-09-14 20:56:54 +0700

    fixing

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ function handleEvent(event) {
     });
   }
   if(incomingMessage[0] === '/tugas'){
+    console.log("mashoookk")
     const return_tugas = async () => {
       try{
         const response = await axios.get("https://pyrgoose.firebaseio.com/tugas.json");
