77912e65b39ec4821b9a4fb5397a9ee308cb9bbd
Galih Fajar <galihfajar00@gmail.com>
2020-10-06 19:26:11 +0700

    fixing firebase url

--- functions/getTugas.js
+++ functions/getTugas.js
@@ -1,23 +1,26 @@
-const axios = require('axios');
+const axios = require("axios");
+const firebaseurl = process.env.FIREBASE_URL;
 
 const getTugas = async (event, client) => {
-    try{
-      const response = await axios.get("https://pyrgoose.firebaseio.com/tugas.json");
-      var msgStr = "";
-      Object.keys(response.data).forEach((key) => {
-        if(msgStr === ""){
-          msgStr = "[LIST TUGAS]\n\n" + "[" + key + "]" + "\n" + response.data[key];
-        }else{
-          msgStr = msgStr + "\n" + "\n" + "[" + key + "]" + "\n" + response.data[key];
-        }
-      });
-      return client.replyMessage(event.replyToken, {
-        type : 'text', text : msgStr
-      });
-    }
-    catch(err){ 
-      new Error("Cannot get tugas!")
-    }
+  try {
+    const response = await axios.get(`${firebaseurl}tugas.json`);
+    var msgStr = "";
+    Object.keys(response.data).forEach((key) => {
+      if (msgStr === "") {
+        msgStr =
+          "[LIST TUGAS]\n\n" + "[" + key + "]" + "\n" + response.data[key];
+      } else {
+        msgStr =
+          msgStr + "\n" + "\n" + "[" + key + "]" + "\n" + response.data[key];
+      }
+    });
+    return client.replyMessage(event.replyToken, {
+      type: "text",
+      text: msgStr,
+    });
+  } catch (err) {
+    new Error("Cannot get tugas!");
   }
+};
 
 module.exports = getTugas;
